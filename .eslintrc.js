60eb1045b0b7bff3203f5fa52c307d0d441e4635
Breyner Sanchez <breinersanchez97@hotmail.com>
2021-09-04 20:39:40 -0400

    add react native and configs vsc files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'no-underscore-dangle': 0,
+    'arrow-body-style': 0,
+    'no-shadow': 0,
+    'consistent-return': 0,
+    'no-nested-ternary': 0,
+    'no-console': 1,
+    'no-case-declarations': 0,
+    'import/prefer-default-export': 0,
+  },
 };
